d232d77fa9ae433afb1ef97b1a7633a6103fb90d
kondoumh <kondoumh@gmail.com>
2022-10-05 18:46:23 +0900

    start broker before test

--- src/test/java/com/kondoumh/kafkaintegrationtest/consumer/EmbeddedKafkaIntegrationTest.java
+++ src/test/java/com/kondoumh/kafkaintegrationtest/consumer/EmbeddedKafkaIntegrationTest.java
@@ -1,42 +1,56 @@
 package com.kondoumh.kafkaintegrationtest.consumer;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.annotation.DirtiesContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+
 @SpringBootTest
 @DirtiesContext
 @EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class EmbeddedKafkaIntegrationTest {
 
   @Autowired
-  public KafkaTemplate<String, String> template;
+  private EmbeddedKafkaBroker broker;
+  
+  private KafkaTemplate<String, String> template;
 
   @Autowired
   private ExampleListener listener;
 
-  private final String topic1 = "TestTopic-1";
+  private static final String TOPIC1 = "TestTopic-1";
 
-  private final String topic2 = "TestTopic-2";
-  
+  private static final String TOPIC2 = "TestTopic-2";
+
+  @BeforeAll
+  void setUp() {
+    template = new KafkaTemplate<>(new DefaultKafkaProducerFactory<String, String>(KafkaTestUtils.producerProps(broker)));
+  }
+
   @Test
   public void recieve() throws Exception {
     String data = "test message1";
-    template.send(topic1, data);
-    Thread.sleep(15000);
+    template.send(TOPIC1, data);
+    Thread.sleep(1000);
     assertThat(listener.getPayload1()).isEqualTo(data);
   }
 
   @Test
   public void recieve2() throws Exception {
     String data = "test message2";
-    template.send(topic2, data);
-    Thread.sleep(15000);
+    template.send(TOPIC2, data);
+    Thread.sleep(1000);
     assertThat(listener.getPayload2()).isEqualTo(data);
   }
 }
